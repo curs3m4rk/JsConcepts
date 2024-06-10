00eda1ed131507c7de27fb28b49c6f3a075a19aa
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-10 23:58:45 +0530

    Hoisting in Action

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,17 @@
 
-getName();
-console.log(x);
-console.log(getName);
-
 var x = 7;
 
 function getName() {
     console.log("India is my Country");
 }
 
+var getName2 = function () {
+
+}
+var getName3 = () => {
+
+}
+
+console.log(x);
+getName();
+console.log(getName);
